fc9f6808a4b5c799f567b512e00833e56fe69492
giuly <giuly@giules.fritz.box>
2025-12-05 19:55:13 +0100

    Initial Versione

---
+++ src/main/java/View/Main.java
@@ -0,0 +1,40 @@
+/**
+ * @file Main.java
+ * @brief Classe di avvio dell'applicazione.
+ *
+ * La classe Main estende l'Application di JavaFX e si occupa di inizializzare
+ * l'ambiente grafico e caricare il layout principale (View).
+ */
+package View;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+public class Main extends Application{
+    
+    /**
+     * @brief Metodo principale per l'avvio dell'applicazione JavaFX.
+     *
+     * Questo metodo viene chiamato dal framework JavaFX dopo l'inizializzazione.
+     * Imposta la scena e mostra la finestra (Stage).
+     *
+     * @param primaryStage Lo stage (finestra) principale dell'applicazione.
+     * @throws Exception Se si verifica un errore durante il caricamento dell'FXML.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+
+    /**
+     * @brief Metodo statico main.
+     *
+     * Metodo standard per l'applicazione Java.
+     *
+     * @param args Array di argomenti passati dalla linea di comando.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
